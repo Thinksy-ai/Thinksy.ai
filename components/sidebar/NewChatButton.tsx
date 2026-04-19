a7905df544c3c89a0ca5d73bd061b2a55047f475
Thinksy-ai <sohamadm.16030@gmail.com>
2026-04-19 16:34:06 +0530

    Create NewChatButton.tsx

---
+++ components/sidebar/NewChatButton.tsx
@@ -0,0 +1,12 @@
+export default function NewChatButton() {
+  return (
+    <button style={{
+      margin: 10,
+      padding: 10,
+      background: "#333",
+      color: "#fff"
+    }}>
+      + New Chat
+    </button>
+  );
+}
