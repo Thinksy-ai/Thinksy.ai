517c5869b3a0fd210c9b22dd2a0c2a5e93cac060
Thinksy-ai <sohamadm.16030@gmail.com>
2026-04-19 16:39:40 +0530

    Update Sidebar.tsx

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,17 +1,20 @@
 import ChatList from "./ChatList";
 import NewChatButton from "./NewChatButton";
 
-export default function Sidebar() {
+export default function Sidebar({
+  chats,
+  activeId,
+  setActiveId,
+  newChat
+}: any) {
   return (
-    <div style={{
-      width: 260,
-      background: "#111",
-      color: "#fff",
-      display: "flex",
-      flexDirection: "column"
-    }}>
-      <NewChatButton />
-      <ChatList />
+    <div style={{ width: 260, background: "#111", color: "#fff" }}>
+      <NewChatButton newChat={newChat} />
+      <ChatList
+        chats={chats}
+        activeId={activeId}
+        setActiveId={setActiveId}
+      />
     </div>
   );
 }
